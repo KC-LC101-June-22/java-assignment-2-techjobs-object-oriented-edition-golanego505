39040deb68019d95350b23053aa153260e794d02
Lane Flint <110209944+golanego505@users.noreply.github.com>
2022-10-16 21:18:02 -0500

    fix new line test 2

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -53,11 +54,11 @@ public class JobTest {
         String data = test_job.toString();
         int length = data.length();
 
-        assertEquals("\n", "\n");
-        assertEquals("\n", "\n");
+        Assert.assertEquals(data.substring(0,1), "\n");
+        Assert.assertEquals(data.substring(length - 1, length), "\n");
 
-        assertEquals(data.substring(0,1), "\n");
-        assertEquals(data.substring(length - 1, length), "\n");
+//        assertEquals(data.substring(0,1), "\n");
+//        assertEquals(data.substring(length - 1, length), "\n");
     }
 
     @Test
